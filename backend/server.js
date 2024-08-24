add3babfe51c617383ce96cb0b1cc595b182e131
Ganesh Benni <ganeshib08@gmail.com>
2024-08-24 21:34:44 +0530

    updated cors poliy

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname=path.resolve();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+  }));
+  
 app.use(express.json());
 app.use(cookiePareser());
 
